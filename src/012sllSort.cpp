daee7ad53af4c90f31d686e3733f7f142235eaaa
Saikrishna-K <Saikrishna-K@missionrnd.com>
2016-03-12 09:20:02 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -1,4 +1,5 @@
-/*
+#include <stdio.h>
+#include <malloc.h>  /*
 OVERVIEW:  Given a single linked list of 0s 1s and 2s ,Sort the SLL such that zeroes 
 will be followed by ones and then twos.
 
@@ -13,8 +14,7 @@ ERROR CASES:
 NOTES: Only 0,1,2, will be in sll nodes
 */
 
-#include <stdio.h>
-#include <malloc.h>
+
 
 struct node {
 	int data;
@@ -22,5 +22,41 @@ struct node {
 };
 
 void sll_012_sort(struct node *head){
-	
+	int one = 0, two = 0;
+	struct node *current = head, *temp, *zerohead = head;
+
+	while (current)
+	{
+
+		int num = current->data;
+		if (current->data == 0)//      if num is equal to zero than append it to linked list
+		{
+			zerohead->data = 0;
+			zerohead = zerohead->next;
+		}
+
+		else if (current->data == 1)// if num is equal to 1 increase the "one" count value
+		{
+			one++;
+
+
+		}
+		else if (current->data == 2)// if num is equal to 2 increase the "two" count value
+		{
+
+			two++;
+		}
+		current = current->next;
+	}
+	while (one--)// appending one's to the linked list
+	{
+		zerohead->data = 1;
+		zerohead = zerohead->next;
+	}
+	while (two--)//appending two's to the linked list
+	{
+		zerohead->data = 2;
+		zerohead = zerohead->next;
+
+	}
 }
